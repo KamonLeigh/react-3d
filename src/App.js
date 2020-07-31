4ec81ed8869c7a6eebfd671cf3c3e12f02f12449
Byron Dunkley <KamonLeigh@users.noreply.github.com>
2020-07-31 21:34:28 +0100

    add orbit control

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import React from 'react';
-import { Canvas } from 'react-three-fiber';
+import { Canvas, useThree, extend } from 'react-three-fiber';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import './App.css';
 
-function Cube() {
+
+// Need to run this outside of react component inorder to use OrbitControl
+extend({ OrbitControls });
+
+function Cube(props) {
   return (
-    <mesh position={[0, 0, 0 ]} rotation={[10, 10, 0]}>
-      <boxBufferGeometry attach="geometry" args={[5, 5, 3]}/>
+    <mesh {...props}>
+      <boxBufferGeometry attach="geometry" args={[2, 2, 1]}/>
       <meshStandardMaterial attach="material" color="orangered"/>
     </mesh>
   )
 }
 
 function Scene() {
+  // domElemnt refers to the canvas
+  const {
+    camera,
+    gl: {
+      domElement
+    }
+  } = useThree();
+
   return (
     <>
       <ambientLight/>
       <pointLight position={[-1, 2, 4]}/>
-      <Cube/>
+      <Cube rotation={[10, 20, 0]} position={[2, 2, 0]}/>
+      <Cube rotation={[10, 10, 0]} position={[0, 0, 0]}/>
+
+      <orbitControls args={[camera, domElement]}/>
     </>
   )
 }
