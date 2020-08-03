2ad55c69d645d3f9a82c24e1ba36eac3875e11ab
Byron Dunkley <KamonLeigh@users.noreply.github.com>
2020-08-03 22:16:34 +0100

    add 3rd party img gltf img to file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import * as THREE from 'three';
-import React, { useState, useRef, useEffect } from 'react';
-import { Canvas, useFrame } from 'react-three-fiber';
+import React, { useState, useRef, useEffect, Suspense } from 'react';
+import { Canvas, useFrame, useLoader } from 'react-three-fiber';
 import { OrbitControls } from 'drei';
-import {TextureLoader} from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import './App.css';
 
 
@@ -27,39 +27,15 @@ import './App.css';
 // sphere args [radius, widthSegments, heightSegmentx]
 // cylinder args [radiusTop, radiusBottom, height, radial segments]
 
-const tempObject = new THREE.Object3D();
 
-function Boxes() {
+function Plant() {
 
   const ref = useRef();
+  const gltf = useLoader(GLTFLoader, '/scene.gltf');
 
-  useFrame((state) => {
-    const time = state.clock.getElapsedTime();
-    const grow = Math.sin(time / 1);
-
-    ref.current.rotation.x += 0.01;
-    ref.current.rotation.y += 0.01;
-
-    let i = 0;
-      for(let x = 0; x < 6; x++) {
-        for(let y = 0; y < 6; y++) {
-          for(let z = 0; z < 6; z++) {
-            const id = i++;
-            tempObject.position.set(3-x * grow , 3-y * grow, 3-z * grow);
-            // build out a grid
-            tempObject.updateMatrix();
-            ref.current.setMatrixAt(id, tempObject.matrix);
-          }
-        }
-      }
-      ref.current.instanceMatrix.needsUpdate = true;
-  })
 
   return (
-    <instancedMesh ref={ref} args={[null, null, 216]}>
-      <boxBufferGeometry attach="geometry" args={[0.7, 0.7, 0.7]}/>
-      <meshPhongMaterial attach="material" color="teal"/>
-    </instancedMesh>
+    <primitive object={gltf.scene} position={[0,0,0]}/>
   )
 }
 
@@ -74,7 +50,9 @@ function Scene() {
     <>
       <ambientLight/>
       <pointLight intensity={0.6} position={[0, 10, 4]}/>
-      <Boxes/>
+      <Suspense fallback={null}>
+        <Plant/>
+      </Suspense>
       <OrbitControls/>
     </>
   )
@@ -84,16 +62,18 @@ function App() {
   return (
     <>
       <Canvas
-        camera={{
-          position: [0, 0, 15],
-          near: 5,
-          far: 20
-        }}
+        // camera={{
+        //   position: [0, 0, 0],
+        // }}
       >
         <Scene/>
       </Canvas>
     </>
   );
 }
+
+/**
+ * near and far is the range which is viewable from the camera
+ */
 
 export default App;
